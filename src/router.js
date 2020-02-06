d11c0f8d6bcc256cf5fb6536aebed011780ffa2d
MiguelGontijo <mpgontijo84@gmail.com>
2020-02-06 02:12:59 -0300

    listando todos os providers

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 import multerConfig from "./config/multer";
 
+import ProviderController from "./app/controllers/ProviderController";
 import ControllerFile from "./app/controllers/ControllerFile";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
@@ -16,8 +17,10 @@ routes.post("/sessions", SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.post("/files", upload.single("file"), ControllerFile.store);
+
 routes.put("/users", UserController.update);
 
-routes.post("/files", upload.single("file"), ControllerFile.store);
+routes.get("/providers", ProviderController.index);
 
 export default routes;
