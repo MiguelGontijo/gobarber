b8f8655c67abf34557ad789354ebb56cbf12d646
MiguelGontijo <mpgontijo84@gmail.com>
2020-02-09 20:02:55 -0300

    GoBarber-Cancelamento e Envio de e-mail-Configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: "796d505463a927",
+        pass: "209b15b85adbf7"
+    },
+    default: {
+        from: "Equipe GoBarber <noreply@gobarber.com>"
+    }
+};
